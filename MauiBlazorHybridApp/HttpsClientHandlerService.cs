f6a88b2ea9c64fc736582d65d9a2c65ec35415e4
Anton <forsakenanton@gmail.com>
2024-05-09 13:17:50 +0300

    Add project files.

---
+++ MauiBlazorHybridApp/HttpsClientHandlerService.cs
@@ -0,0 +1,41 @@
+﻿namespace MauiBlazorHybridApp;
+
+
+// (!) https://learn.microsoft.com/ru-ru/dotnet/maui/data-cloud/local-web-services?view=net-maui-8.0#bypass-the-certificate-security-check
+public class HttpsClientHandlerService
+{
+    public HttpMessageHandler GetPlatformMessageHandler()
+    {
+#if ANDROID
+        var handler = new Xamarin.Android.Net.AndroidMessageHandler();
+        handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+        {
+            if (cert != null && cert.Issuer.Equals("CN=localhost"))
+                return true;
+
+            return errors == System.Net.Security.SslPolicyErrors.None;
+        };
+
+        return handler;
+#elif IOS
+        var handler = new NSUrlSessionHandler
+        {
+            TrustOverrideForUrl = IsHttpsLocalhost
+        };
+
+        return handler;
+#else
+        throw new PlatformNotSupportedException("Only Android and iOS supported.");
+#endif
+    }
+
+#if IOS
+    public bool IsHttpsLocalhost(NSUrlSessionHandler sender, string url, Security.SecTrust trust)
+    {
+        if (url.StartsWith("https://localhost"))
+            return true;
+
+        return false;
+    }
+#endif
+}
---
+++ MauiBlazorHybridApp/MauiProgram.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiBlazorHybridApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+            // Create HttpClient
+            builder.Services.AddScoped(sp =>
+            {
+                Uri baseAddress;
+
+                // Кожен екземпляр Android Emulator ізольований
+                // від мережевих інтерфейсів комп'ютера розробки
+                // за допомогою віртуального маршрутизатора.
+                // Таким чином, емульований пристрій не може бачити
+                // комп'ютер розробки або інші екземпляри емулятора в мережі.
+                // Віртуальний маршрутизатор кожного емулятора керує
+                // спеціалізованим мережевим простором, що має
+                // попередньо виділені адреси, а адреса 10.0.2.2
+                // є псевдонімом для інтерфейсу замикання вузла на себе
+                // (127.0.0.1 на комп'ютері розробки).
+                // https://learn.microsoft.com/ru-ru/dotnet/maui/data-cloud/local-web-services?view=net-maui-8.0#android
+                if (DeviceInfo.Platform == DevicePlatform.Android)
+                {
+                    baseAddress = new Uri("https://10.0.2.2:7217");
+                }
+                else if (DeviceInfo.Platform == DevicePlatform.WinUI)
+                {
+                    baseAddress = new Uri("https://localhost:7217");
+                }
+                else
+                {
+                    throw new PlatformNotSupportedException();
+                }
+
+#if DEBUG
+                // (!) Довіряємо сертифікату, без цього запит до API з Android НЕ запрацює
+                // https://learn.microsoft.com/ru-ru/dotnet/maui/data-cloud/local-web-services?view=net-maui-8.0#bypass-the-certificate-security-check
+                HttpsClientHandlerService handler = new HttpsClientHandlerService();
+
+                return new HttpClient(handler.GetPlatformMessageHandler()) // (!)
+                {
+                    BaseAddress = baseAddress 
+                };
+#else
+                return new HttpClient("Qwerty");
+#endif
+            });
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
